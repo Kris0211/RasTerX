4afa3711d5f256725bdffd2c554ef407a30e714e
Kris0211 <stawinogakris@gmail.com>
2025-04-18 23:27:12 +0200

    Simplify Ray

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -1,28 +1,25 @@
 #include "../include/Ray.hpp"
 namespace rtx
 {
-	Ray::Ray() : origin(Vector3::Zero()), direction(Vector3::Zero()), distance(0.f)
+	Ray::Ray() 
+		: origin(Vector3::Zero()), direction(Vector3::Zero()), distance(0.f)
 	{}
 
-	Ray::Ray(const Vector3& origin, const Vector3& destination)
-		: origin(origin), destination(destination)
-	{
-		direction = (destination - origin).Normal();
-		//distance = (destination - origin).Length();
-		distance = FLT_MAX;
-	}
-
 	Ray::Ray(const Vector3& origin, const Vector3& direction, const float distance) 
-		: origin(origin), direction(direction), distance(distance)
+		: origin(origin), direction(direction.Normal()), distance(distance)
+	{}
+
+	Vector3 Ray::PointAt(const float t) const
 	{
-		destination = origin + this->direction * distance;
+		return origin + direction * t;
 	}
 
 	std::string Ray::ToString() const
 	{
 		std::stringstream ss;
-		ss << "{\nOrigin: " << this->origin.ToString() << "\nDirection: " << this->direction.ToString()
-			<< "\nDestination: " << this->destination.ToString() << "\nDistance: " << this->distance << "\n}";
+		ss << "{\nOrigin: " << this->origin.ToString() 
+			<< "\nDirection: " << this->direction.ToString()
+			<< "\nDistance: " << this->distance << "\n}";
 		return ss.str();
 	}
 }
